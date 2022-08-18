efef80c51f1262e5c2c41778fbc62cb53893ebda
Abeer Ahmed <abeerahmad389@gmail.com>
2022-08-18 17:55:38 +0500

    frist

---
+++ server/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const cors = require("cors");
+
+app.use(cors());
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+});
+let users = [];
+socketIO.on("connection", (socket) => {
+  console.log(`⚡: ${socket.id} user just connected!`);
+  //sends the message to all the users on the server
+  socket.on("message", (data) => {
+    console.log(data);
+    socketIO.emit("messageResponse", data);
+  });
+
+  socket.on("typing", (data) => socket.broadcast.emit("typingResponse", data));
+
+  //Listens when a new user joins the server
+  socket.on("newUser", (data) => {
+    //Adds the new user to the list of users
+    users.push(data);
+    console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+    //Updates the list of users when a user disconnects from the server
+    users = users.filter((user) => user.socketID !== socket.id);
+    console.log(users);
+    //Sends the list of users to the client
+    socketIO.emit("newUserResponse", users);
+    socket.disconnect();
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world",
+  });
+});
+
+http.listen(4000, () => {
+  console.log(`Server listening on ${4000}`);
+});
